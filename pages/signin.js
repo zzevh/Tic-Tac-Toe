3e7915223e1a803f8f131163c857cfbc68c5a71d
zhv.blaze <zehvdev@gmail.com>
2024-10-11 22:11:04 +0200

    teraz jest dobrze

--- pages/signin.js
+++ pages/signin.js
@@ -5,6 +5,7 @@ import Router from "next/router";
 import { v4 as uuidv4 } from 'uuid';
 
 export default function Signin() {
+
     const [name, setName] = useState('');
 
     useEffect(() => {
@@ -13,26 +14,6 @@ export default function Signin() {
             getFromStorage('player-name') === '')) {
             Router.push('/');
         }
-    }, []);
-
-    useEffect(() => {
-        // Dynamically load AdSense script when the component mounts
-        const script = document.createElement('script');
-        script.async = true;
-        script.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js";
-        script.setAttribute("data-ad-client", "ca-pub-2086544123628687");
-        script.crossOrigin = "anonymous";
-        document.body.appendChild(script);
-
-        // Initialize ads after script is loaded
-        script.onload = () => {
-            (window.adsbygoogle = window.adsbygoogle || []).push({});
-        };
-
-        return () => {
-            // Cleanup the script if component unmounts
-            document.body.removeChild(script);
-        };
     }, []);
 
     const handleSubmit = (event, action = '') => {
@@ -77,19 +58,11 @@ export default function Signin() {
                     <div className='footer d-block w-full flex xs:justify-center justify-end mt-5'>
                         <button onClick={e => handleSubmit(e, 'click')} className='bg-gradient-shadow relative rounded-full xs:w-full px-7 py-2 bg-gradient-to-tr from-[#2475C5] via-[#39527B] to-[#206ABD] text-base font-medium text-white hover:opacity-90 focus:outline-none focus:ring-4 focus:ring-offset-0 focus:ring-[#206ABD]'>Submit</button>
                     </div>
-                </div>
-
-                {/* AdSense Block */}
-                <div>
-                    <ins className="adsbygoogle"
-                        style={{ display: 'block', width: '100%', height: '300px', minWidth: '300px' }}
-                        data-ad-client="ca-pub-2086544123628687"
-                        data-ad-slot="7029626634"
-                        data-ad-format="auto"
-                        data-full-width-responsive="true">
-                    </ins>
 
                 </div>
+
+
+
             </div>
         </AnimatePage>
     )
