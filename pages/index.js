918b34cebffd464c8a336cf6cf55cb8acdc795d9
zhv.blaze <zehvdev@gmail.com>
2024-10-11 20:04:22 +0200

    Initial commit

--- pages/index.js
+++ pages/index.js
@@ -587,6 +587,12 @@ export default function Home() {
                     </div>
                   </div>
 
+<<<<<<< HEAD
+=======
+
+                  <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2086544123628687"
+                    crossorigin="anonymous"></script>
+>>>>>>> 100d49ae (Initial commit)
 
                   <div className="flex items-center">
                     <div className={"flex w-28 flex-col justify-end  " + (!isMyTurn ? "" : "opacity-50")}>
