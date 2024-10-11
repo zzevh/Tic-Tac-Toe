845290227cefc568e5f786f2f9c2e005b28d78b4
zhv.blaze <zehvdev@gmail.com>
2024-10-11 22:18:12 +0200

    mayby?

--- pages/signin.js
+++ pages/signin.js
@@ -1,18 +1,37 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import AnimatePage from '../components/AnimatePage';
 import { getFromStorage, setToStorage } from '../helper/localStorage';
 import Router from "next/router";
 import { v4 as uuidv4 } from 'uuid';
 
 export default function Signin() {
-
     const [name, setName] = useState('');
+    const adInitialized = useRef(false); // Ref to track if the ad has been initialized
 
     useEffect(() => {
         if (!(typeof getFromStorage('player-name') === 'undefined' ||
             getFromStorage('player-name') === null ||
             getFromStorage('player-name') === '')) {
             Router.push('/');
+        }
+    }, []);
+
+    useEffect(() => {
+        // Load the AdSense script only once
+        if (!adInitialized.current) {
+            const script = document.createElement('script');
+            script.async = true;
+            script.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js";
+            script.setAttribute("data-ad-client", "ca-pub-2086544123628687");
+            script.crossOrigin = "anonymous";
+            document.body.appendChild(script);
+
+            // Initialize ads after script is loaded
+            script.onload = () => {
+                (window.adsbygoogle = window.adsbygoogle || []).push({});
+            };
+
+            adInitialized.current = true; // Mark that the ad has been initialized
         }
     }, []);
 
@@ -58,11 +77,18 @@ export default function Signin() {
                     <div className='footer d-block w-full flex xs:justify-center justify-end mt-5'>
                         <button onClick={e => handleSubmit(e, 'click')} className='bg-gradient-shadow relative rounded-full xs:w-full px-7 py-2 bg-gradient-to-tr from-[#2475C5] via-[#39527B] to-[#206ABD] text-base font-medium text-white hover:opacity-90 focus:outline-none focus:ring-4 focus:ring-offset-0 focus:ring-[#206ABD]'>Submit</button>
                     </div>
-
                 </div>
 
-
-
+                {/* AdSense Block */}
+                <div>
+                    <ins className="adsbygoogle"
+                        style={{ display: 'block', width: '100%', height: 'auto', minWidth: '300px', minHeight: '250px' }}
+                        data-ad-client="ca-pub-2086544123628687"
+                        data-ad-slot="7029626634"
+                        data-ad-format="auto"
+                        data-full-width-responsive="true">
+                    </ins>
+                </div>
             </div>
         </AnimatePage>
     )
